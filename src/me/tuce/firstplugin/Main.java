f702dc67ddca2ace618e9b90e93c2260296931af
dev-stan <67323109+dev-stan@users.noreply.github.com>
2021-07-30 04:54:28 +0200

    Create config file for future use
    Add customizable config file, the code I isn't the default way to add a config file but can be used to create multiple files which you can add later. Please test before implementing to make sure everything is working correctly.

--- src/me/tuce/firstplugin/Main.java
+++ src/me/tuce/firstplugin/Main.java
@@ -7,6 +7,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
+
+    private File customConfigFile;
+    private FileConfiguration customConfig;
+
     @Override
     public void onEnable(){
         // Set command executors
@@ -16,5 +20,24 @@ public class Main extends JavaPlugin implements Listener {
         this.getCommand("sellhstack").setExecutor(sellItem);
         this.getCommand("cost").setExecutor(new Cost());
         this.getCommand("buy").setExecutor(new Buy());
+    }
+    
+        public FileConfiguration getCustomConfig() {
+        return this.customConfig;
+    }
+
+    private void createCustomConfig() {
+        customConfigFile = new File(getDataFolder(), "config.yml");
+        if (!customConfigFile.exists()) {
+            customConfigFile.getParentFile().mkdirs();
+            saveResource("config.yml", false);
+        }
+
+        customConfig= new YamlConfiguration();
+        try {
+            customConfig.load(customConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 }
