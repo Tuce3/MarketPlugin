afd8dede7e7ae34bd1aa905a3126ddeb447726fd
Tuce3 <69303354+Tuce3@users.noreply.github.com>
2021-07-30 22:42:14 +0200

    Its working, only missed imports

--- src/me/tuce/firstplugin/Main.java
+++ src/me/tuce/firstplugin/Main.java
@@ -3,8 +3,14 @@ package me.tuce.firstplugin;
 import me.tuce.firstplugin.commands.Buy;
 import me.tuce.firstplugin.commands.Cost;
 import me.tuce.firstplugin.commands.SellItem;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -13,16 +19,19 @@ public class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable(){
+        
+        createCustomConfig();
+        
         // Set command executors
-        SellItem sellItem = new SellItem();
+        SellItem sellItem = new SellItem(this);
         this.getCommand("sell").setExecutor(sellItem);
         this.getCommand("sellstack").setExecutor(sellItem);
         this.getCommand("sellhstack").setExecutor(sellItem);
         this.getCommand("cost").setExecutor(new Cost());
         this.getCommand("buy").setExecutor(new Buy());
     }
-    
-        public FileConfiguration getCustomConfig() {
+
+    public FileConfiguration getCustomConfig() {
         return this.customConfig;
     }
 
@@ -33,7 +42,7 @@ public class Main extends JavaPlugin implements Listener {
             saveResource("config.yml", false);
         }
 
-        customConfig= new YamlConfiguration();
+        customConfig = new YamlConfiguration();
         try {
             customConfig.load(customConfigFile);
         } catch (IOException | InvalidConfigurationException e) {
