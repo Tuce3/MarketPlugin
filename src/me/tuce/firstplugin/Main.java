35ef209d76cb3ec0ef84b98133ca5f31eae678c1
BuildTools <tuxe5335@gmail.com>
2021-09-03 21:39:41 +0200

    Added check to print in console if item name in blacklist is incorrect.

--- src/me/tuce/firstplugin/Main.java
+++ src/me/tuce/firstplugin/Main.java
@@ -3,6 +3,8 @@ package me.tuce.firstplugin;
 import me.tuce.firstplugin.commands.Buy;
 import me.tuce.firstplugin.commands.Cost;
 import me.tuce.firstplugin.commands.SellItem;
+import me.tuce.firstplugin.helper.InputCheck;
+import org.bukkit.Material;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -11,6 +13,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -28,6 +32,17 @@ public class Main extends JavaPlugin implements Listener {
         this.getCommand("sellhstack").setExecutor(sellItem);
         this.getCommand("cost").setExecutor(new Cost());
         this.getCommand("buy").setExecutor(new Buy());
+
+        // Check whether all blacklist item names are correct in config.yml
+        List<?> blacklist = this.getCustomConfig().getList("blacklist");
+        for (ListIterator<?> it = blacklist.listIterator(); it.hasNext(); ) {
+            String itemName = (String) it.next();
+            try {
+                Material.valueOf(itemName);
+            } catch (IllegalArgumentException argumentException) {
+                System.out.println("[Market] " + itemName + " is not a valid item(blacklist)");
+            }
+        }
     }
 
     public FileConfiguration getCustomConfig() {
